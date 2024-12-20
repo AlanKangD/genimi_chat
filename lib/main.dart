150d9f5f6bbad753e80cecfe05f515659a09479a
alan <kno12350@gmail.com>
2024-12-20 09:36:33 +0900

    safeArea 를 설정하면 위 아래의 안전영역을 설정하여 프레임을 넘어서는것을 방지할 수 있다

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          colorSchemeSeed: const Color.fromARGB(255, 0, 255, 255),
-          brightness: Brightness.dark),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Genimi"),
-        ),
-        body: const Center(
-          child: Chatroom(),
+    // safeArea 를 설정하면 위 아래의 안전영역을 설정하여 프레임을 넘어서는것을 방지할 수 있다.
+    return SafeArea(
+      child: MaterialApp(
+        // 상단 debug 표시 삭제 or 표시ㄱ
+        // debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            colorSchemeSeed: const Color.fromARGB(255, 0, 255, 255),
+            brightness: Brightness.dark),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Genimi"),
+          ),
+          body: const Center(
+            child: Chatroom(),
+          ),
         ),
       ),
     );
